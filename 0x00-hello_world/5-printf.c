69c96f2a80719c11bd24e7ac023518baf4c851a1
Shegun719 <Shegun719@gmail.com>
2022-09-13 00:09:58 -0700

    added new  files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,19 @@
 #include <stdio>
 
 /**
- *main -function
+ *main - function
  *
  *Description: a program that prints
  *"Programming is like building a multilingual puzzle
- *Return: 0;
+ *Return: o;
+ *
  */
+
 int main(void)
 {
-  
-  puts("\"Programming is like building a multilingual puzzle");
 
-    Return(0);
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+
+  return(0);
 
 }
