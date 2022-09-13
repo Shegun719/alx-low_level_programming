0da28511afcc484923d92ce6dd3c97f6dbb9c7ec
Shegun719 <Shegun719@gmail.com>
2022-09-13 01:27:41 -0700

    corrected file 4,5 and 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,9 +9,9 @@
  */
 int main(void)
 {
-  
-  puts("\"Programming is like building a multilingual puzzle");
 
-    Return(0);
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 
 }
