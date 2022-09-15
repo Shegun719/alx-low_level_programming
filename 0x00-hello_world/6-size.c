5888539753637d449e37abe35ab82dca4bab2646
Shegun719 <Shegun719@gmail.com>
2022-09-15 15:57:39 -0700

    new correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,4 +17,6 @@ printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
+return (0);
+
 }
