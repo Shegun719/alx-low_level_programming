1b213d1067bd3b18abaf655ade51852a186d0314
Shegun719 <Shegun719@gmail.com>
2022-09-20 02:02:57 -0700

    added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - prints the lowercase alphabet in reverse
+ *
+ *Return: 0;
+ *
+ */
+
+int main(void)
+{
+char num;
+
+for (num = 10; num >= 0; num--)
+putchar(num);
+
+return (0);
+
+}
