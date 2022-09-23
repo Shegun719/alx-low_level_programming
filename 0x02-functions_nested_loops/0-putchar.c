98b637792a414824612d63edb68973a91f402a50
Shegun719 <Shegun719@gmail.com>
2022-09-23 14:09:52 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main -  a program that prints _putchar, followed by a new line.
+ *
+ *Return: 0;
+ *
+ */
+
+int main(void)
+{
+
+printf("_putchar");
+
+return (0);
+
+}
