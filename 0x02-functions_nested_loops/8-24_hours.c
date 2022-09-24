640248e7c598407a5748a28925a6e82ee4e56813
Shegun719 <Shegun719@gmail.com>
2022-09-24 05:45:10 -0700

    correctted 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,13 +17,13 @@ int H, m;
 
 for (H = 0; H < 24; H++)
 for (m = 0; m <  60; m++)
-
+{
 _putchar((H / 10)  + '0');
 _putchar((H % 10) + '0');
 _putchar(':');
 _putchar((m / 10) + '0');
 _putchar((m % 10) + '0');
 _putchar('\n');
-
+}
 
 }
