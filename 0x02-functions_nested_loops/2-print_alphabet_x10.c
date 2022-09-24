ecd44102cbe6dcd582b169d4e32e9053deed0764
Shegun719 <Shegun719@gmail.com>
2022-09-23 17:32:49 -0700

    added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "print_alphabet_x10"
+
+/**
+ *print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ *Return: nothing;
+ *
+ */
+
+void print_alphabet_x10(void)
+
+int c, ch;
+ch = 0;
+while (ch < 10)
+
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+
+ch++;
+
+
+_putchar('\n');
+
+}
