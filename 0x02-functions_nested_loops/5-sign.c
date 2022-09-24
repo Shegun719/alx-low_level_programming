4abc5d3570b5f66083d54b1320bed8f241208221
Shegun719 <Shegun719@gmail.com>
2022-09-23 22:48:08 -0700

    corrected 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,18 +12,12 @@
 int print_sign(int n)
 {
 
-char *ch = "+";
-char *sh = "0";
-char *xh = "-";
-
 if (n > 0)
 _putchar(*ch);
 return (1);
-
 else if (n == 0)
 _putchar(*sh);
 return (0);
-
 else (n < 0)
 _putchar(*xh);
 return (-1);
