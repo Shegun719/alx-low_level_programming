e3a1c02f9ef1ff4609ea8b46e1a618770e16ae6f
Shegun719 <Shegun719@gmail.com>
2022-09-25 15:58:09 -0700

    added 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,43 @@
 #include "main.h"
 
+/**
+ * times_table - prints the times table from 0 - 9.
+ *
+ * Return: Nothing.
+ */
+
+void times_table(void)
+{
+
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = (x * y);
+			if (y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (y >= 10)
+			{
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+			else if (z < 10 && y != 0)
+			{
+				_putchar(' ');
+				_putchar((z % 10) + '0');
+			}
+			else
+				_putchar((z % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
 
 
 
