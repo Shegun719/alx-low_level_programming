283964eacb8874c5079a699b84d2b6a8318117dd
Shegun719 <Shegun719@gmail.com>
2022-09-25 19:55:13 -0700

    made new corrections

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int largest_number(int a, int b, int c)
+
+int main(void)
+{
+
+  int largest_number(int a, int b, int c)
+{
+    int largest;
+
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
+
+  
+        int a, b, c;
+        int largest;
+
+        a = 972;
+        b = -98;
+        c = 0;
+
+        largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
+
+        return (0);
+
+}
